9419bb55dfa7d5a79f4e3824b727265dac07911e
SasoPavlic <saso.pavlic3@gmail.com>
2023-04-18 16:01:08 +0200

    PoC - Added test datalaoder and testing method.

--- dataloaders/images.py
+++ dataloaders/images.py
@@ -1,24 +1,29 @@
 import os
 from typing import List, Optional, Union
 import pandas as pd
-import torch
+
 from lightning.pytorch import LightningDataModule
-from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader, Dataset
+
+from torch.utils.data import Dataset
+from lightning.pytorch.utilities.data import DataLoader
+
 from torchvision import transforms
 
 from .transforms import *
 
 
-class Transformer_train(Dataset):
+class Transformer_NYU2(Dataset):
     def __init__(self, csv_file, transform=None):
         """INTERNET CODE"""
 
         # TODO rename/replace to x_train
+        # TODO Add path to root folder
         csv_file_path = os.getcwd() + csv_file
         print(csv_file_path)
         self.paths = pd.read_csv(csv_file_path, header=None,
                                  names=['image', 'depth'])
+
+        self.paths = self.paths.head(256)
 
         self.transform = transform
 
@@ -35,10 +40,11 @@ class Transformer_train(Dataset):
         # self.y_train = torch.tensor(self.y_train[:].values)
         # self.x_train = torch.tensor(df_train[:].values)
         # https://stackoverflow.com/questions/50307707/convert-pandas-dataframe-to-pytorch-tensor
-        super(Transformer_train, self).__init__()
+        super(Transformer_NYU2, self).__init__()
 
     def __len__(self):
         return len(self.paths)
+
     def __getitem__(self, index: int) -> dict:
         image = Image.open(self.paths['image'][index])
         depth = Image.open(self.paths['depth'][index])
@@ -75,11 +81,6 @@ class NYUDataset(LightningDataModule):
         self.anomaly_label = anomaly_label
 
     def setup(self, stage: Optional[str] = None) -> None:
-        pass
-
-    # TODO Implement re-usable datalaoder process
-    # https://github.com/pytorch/pytorch/issues/15849#issuecomment-573921048
-    def train_dataloader(self) -> DataLoader:
         __imagenet_pca = {
             'eigval': torch.Tensor([0.2175, 0.0188, 0.0045]),
             'eigvec': torch.Tensor([
@@ -96,7 +97,7 @@ class NYUDataset(LightningDataModule):
                 Scale(240),
                 RandomHorizontalFlip(),
                 RandomRotate(5),
-                CenterCrop([304, 228], [152, 114]),
+                CenterCrop([304, 304], [152, 114]),
                 ToTensor(),
                 Lighting(0.1, __imagenet_pca[
                     'eigval'], __imagenet_pca['eigvec']),
@@ -110,15 +111,49 @@ class NYUDataset(LightningDataModule):
 
             ]
         )
-        self.train_dataset = Transformer_train(
-            csv_file=self.data_path,
+        self.train_dataset = Transformer_NYU2(
+            csv_file=self.data_path + "nyu2_train.csv",
             transform=train_transform)
 
+        __imagenet_stats = {'mean': [0.485, 0.456, 0.406],
+                            'std': [0.229, 0.224, 0.225]}
+
+        test_transform = transforms.Compose(
+            [
+                Scale(240),
+                CenterCrop([304, 304], [304, 304]),
+                ToTensor(is_test=True),
+                Normalize(__imagenet_stats['mean'],
+                          __imagenet_stats['std'])
+            ]
+
+        )
+
+        self.test_dataset = Transformer_NYU2(
+            csv_file=self.data_path + "nyu2_test.csv",
+            transform=test_transform)
+
+    # TODO Implement re-usable datalaoder process
+    # https://github.com/pytorch/pytorch/issues/15849#issuecomment-573921048
+
+    def train_dataloader(self) -> DataLoader:
         data = DataLoader(
             self.train_dataset,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             shuffle=True,
+            pin_memory=self.pin_memory,
+            # persistent_workers=True
+        )
+
+        return data
+
+    def test_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        data = DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            shuffle=False,
             pin_memory=self.pin_memory,
             # persistent_workers=True
         )
