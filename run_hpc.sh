8eec6c370b1ea4c8522530142741a92d828e142b
SasoPavlic <saso.pavlic3@gmail.com>
2023-06-05 14:09:57 +0200

    Modified config file 2

--- run_hpc.sh
+++ run_hpc.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 ## Running code on SLURM cluster
 ##https://pytorch-lightning.readthedocs.io/en/stable/clouds/cluster_advanced.html
-#SBATCH -J nianet
+#SBATCH -J nianet-cae
 #SBATCH -o nianet-cae-%j.out
 #SBATCH -e nianet-cae-%j.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --partition=gpu
-#SBATCH --mem-per-gpu=32GB
+#SBATCH --mem-per-gpu=32GB  # memory per GPU
+#SBATCH --mem-per-cpu=8GB
 #SBATCH --gres=gpu:1
 #SBATCH --time=72:00:00
 
 #singularity exec -e --pwd /app -B /ceph/grid/home/sasop/Result-long:/app/Result,/ceph/grid/home/sasop/checkpoint-long:/app/checkpoint --nv docker://spartan300/urv-model:1 python ./train.py
-singularity exec -e --pwd /app -B $(pwd)/logs:/app/logs,$(pwd)/data:/app/data --nv docker://spartan300/nianet:cae python main.py
+singularity exec -e --pwd /app -B $(pwd)/logs:/app/logs,$(pwd)/data:/app/data,$(pwd)/configs:/app/configs --nv docker://spartan300/nianet:cae python main.py
